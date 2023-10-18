8be950dac198a20058c1889a8455852b9bdd098b
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-18 16:56:13 +0200

    added-single.members-in-DOM-card

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -4,37 +4,37 @@ const teamMembersArray = [
     {
         name: 'Wayne Barnett',
         position: 'Founder & CEO',
-        photo: '<img src="./img/wayne-barnett-founder-ceo.jpg" alt="">'
+        photo: './img/wayne-barnett-founder-ceo.jpg'
     },
 
     {
         name: 'Angela Caroll',
         position: 'Chief Editor',
-        photo: 'angela-caroll-chief-editor.jpg'
+        photo: './img/angela-caroll-chief-editor.jpg'
     },
 
     {
         name: 'Walter Gordon',
         position: 'Office Manager',
-        photo: 'walter-gordon-office-manager.jpg'
+        photo: './img/walter-gordon-office-manager.jpg'
     },
 
     {
         name: 'Angela Lopez',
         position: 'Social Media Manager',
-        photo: 'angela-lopez-social-media-manager.jpg'
+        photo: './img/angela-lopez-social-media-manager.jpg'
     },
 
     {
         name: 'Scott Estrada',
         position: 'Developer',
-        photo: 'scott-estrada-developer.jpg'
+        photo: './img/scott-estrada-developer.jpg'
     },
 
     {
         name: 'Barbara Ramos',
         position: 'Graphic Designer',
-        photo: 'barbara-ramos-graphic-designer.jpg'
+        photo: './img/barbara-ramos-graphic-designer.jpg'
     },
 ];
 
@@ -49,9 +49,20 @@ let memberPhoto = ''
 for (let i = 0; i < teamMembersArray.length; i++) {
     const infoTeam = teamMembersArray[i];
     // console.log(teamMembersArray[i]);
-    memberName += infoTeam.name;
-    memberPosition += infoTeam.position;
-    memberPhoto += infoTeam.photo;
+    memberName = infoTeam.name;
+    memberPosition = infoTeam.position;
+    memberPhoto = infoTeam.photo;
+
+    const cardDomElement = document.getElementById('card');
+    console.log(cardDomElement);
+
+    const cardString = `
+    <div class="col">
+    <img src="${memberPhoto}" alt="">
+    <p>${memberName}</p>
+    <p>${memberPosition}</p>`
+
+    cardDomElement.innerHTML += cardString;
 }
 
 console.log(memberName);
@@ -59,7 +70,7 @@ console.log(memberName);
 
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-// const cardDomElement = document.getElementById('card');
-// console.log(cardDomElement);
-// cardDomElement.innerHTML = memberName;
+
+
+
 
